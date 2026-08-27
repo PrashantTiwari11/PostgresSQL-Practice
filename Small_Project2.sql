de194936fd72bff0f67656e01f56ae9b4bd28fc1
PRASHANT TIWARI <pt867987@gmail.com>
2026-08-27 22:45:53 +0530

    Create orders table and insert customer data
    Added orders table with foreign key constraints and inserted customer data.

--- Small_Project2.sql
+++ Small_Project2.sql
@@ -18,3 +18,27 @@ CREATE TABLE products (
     category VARCHAR(50),
     price DECIMAL(10,2)
 );
+
+3️⃣ Orders Table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    product_id INT,
+    quantity INT,
+    order_date DATE,
+    
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+Step 4: Customers Data Insert Karo
+INSERT INTO customers (customer_id, customer_name, city, age)
+VALUES
+(1, 'Rahul Sharma', 'Delhi', 25),
+(2, 'Priya Patel', 'Mumbai', 30),
+(3, 'Amit Kumar', 'Surat', 22),
+(4, 'Neha Singh', 'Delhi', 28),
+(5, 'Rohit Shah', 'Ahmedabad', 35),
+(6, 'Sneha Verma', 'Mumbai', 27),
+(7, 'Vikas Patel', 'Surat', 32),
+(8, 'Anjali Gupta', 'Delhi', 24);
